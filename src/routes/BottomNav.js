f3688c39c68f9e3310f572a345352413286ada76
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-07-28 13:54:03 +0200

    i dunno

--- src/routes/BottomNav.js
+++ src/routes/BottomNav.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
 import Favorites from "../screens/Favorites";
 import Search from "../screens/Search";
 import GlobalStyles from "../shared/GlobalStyles";
@@ -8,8 +9,25 @@ import NavTab from "../components/NavTab";
 import HomeStack from "./HomeStack";
 import IssueStack from "./IssueStack";
 import i18n from "../i18n";
+import expoPushTokens from "../http/expoPushTokens";
 
 const { Navigator, Screen } = createBottomTabNavigator();
+
+// impliment push notifications
+const registerForPushNotifications = async () => {
+  try {
+    const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (!permission.granted) return;
+    const token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+  } catch (error) {
+    console.log("Error getting a token", error);
+  }
+};
+
+useEffect(() => {
+  registerForPushNotifications();
+}, []);
 
 const BottomNav = () => {
   return (
